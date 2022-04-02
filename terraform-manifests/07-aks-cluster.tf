32d0012a38506a5768d4cfaa3ff27f6109667a1d
yby227 <56344777+yby227@users.noreply.github.com>
2022-04-02 17:57:48 +0800

    Update 07-aks-cluster.tf

--- terraform-manifests/07-aks-cluster.tf
+++ terraform-manifests/07-aks-cluster.tf
@@ -53,10 +53,10 @@ role_based_access_control {
 }  
 
 # Windows Admin Profile
-windows_profile {
-  admin_username            = var.windows_admin_username
-  admin_password            = var.windows_admin_password
-}
+#windows_profile {
+#  admin_username            = var.windows_admin_username
+#  admin_password            = var.windows_admin_password
+#}
 
 # Linux Profile
 linux_profile {
